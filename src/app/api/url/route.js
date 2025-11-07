bfb0c3f0478d7ca5bede9fd0eaf726c6467163dc
Satyabrata Saha <108542741+satyabrata-saha@users.noreply.github.com>
2025-11-08 00:20:56 +0530

    "Using neondb store data"

--- src/app/api/url/route.js
+++ src/app/api/url/route.js
@@ -1,20 +1,36 @@
 import { NextResponse } from "next/server";
+import { nanoid } from "nanoid";
+import { neon } from "@neondatabase/serverless";
 
-export async function GET() {}
+export async function GET() {
+  const sql = neon(process.env.DATABASE_URL);
+
+  const response = await sql`SELECT * FROM short_url ORDER BY id DESC`;
+
+  return NextResponse.json({
+    status: 200,
+    message: "Data Fetched Successfully",
+    data: response,
+  });
+}
 
 export async function POST(request = NextRequest) {
   const searchData = await request.json();
   const url = searchData.url;
+  const randomString = nanoid(6);
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const sql = neon(process.env.DATABASE_URL);
 
   try {
-    const randomUrlPath = Math.floor(Math.random() * 100);
+    const createDataInDatabase =
+      await sql`INSERT INTO short_url (originalURL, shortURL) VALUES (${url}, ${randomString}) RETURNING *`;
 
-    const urlData = {
-      shortURL: process.env.BASE_URL + randomUrlPath,
-      originalURL: url,
+    const data = {
+      id: createDataInDatabase[0].id,
+      shorturl: baseUrl + createDataInDatabase[0].shorturl,
+      originalurl: createDataInDatabase[0].originalurl,
     };
-
-    const data = urlData;
+    console.log(data);
 
     return NextResponse.json({
       status: 200,
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [originalURL, setOriginalURL] = useState("");
-  const [urlData, setUrlData] = useState({});
+  const [isCopied, setIsCopied] = useState(false);
+  const [urlData, setUrlData] = useState([]);
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -19,12 +21,36 @@ export default function Home() {
         body: JSON.stringify({ url: event.target.url.value }),
       });
 
-      const data = (await response.json()).data;
-      setUrlData(data);
+      const data = await response.json();
+      setUrlData([data.data]);
+      console.log([data.data]);
     } catch (error) {
       console.error("Error submitting URL:", error);
     }
   };
+
+  const handleCopy = (urlToCopy) => {
+    navigator.clipboard.writeText(urlToCopy);
+  };
+
+  useEffect(() => {
+    const firstRequest = async () => {
+      try {
+        const response = await fetch("/api/url/", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await response.json();
+        setUrlData([data.data]);
+        console.log([data.data]);
+      } catch (error) {
+        console.error("Error submitting URL:", error);
+      }
+    };
+    firstRequest();
+  }, []);
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -40,9 +66,63 @@ export default function Home() {
             Submit
           </button>
         </form>
-        {/* <p>{originalURL}</p> */}
-        <p>{urlData.shortURL}</p>
-        <p>{urlData.originalURL}</p>
+        <div className="overflow-x-auto rounded-lg border border-slate-700">
+          <table className="min-w-full divide-y divide-slate-700">
+            <thead className="bg-slate-900">
+              <tr>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-slate-400"
+                >
+                  Original URL
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-slate-400"
+                >
+                  Short URL
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-right text-xs font-medium uppercase tracking-wider text-slate-400"
+                >
+                  Action
+                </th>
+              </tr>
+            </thead>
+
+            {/* Table Body */}
+            <tbody className="divide-y divide-slate-700 bg-slate-800">
+              {urlData[0]?.map((data) => (
+                <tr key={data.id}>
+                  <td className="whitespace-nowrap px-6 py-4 text-sm text-slate-300">
+                    {/* Added truncation for very long URLs */}
+                    <div className="max-w-xs truncate" title={data.originalurl}>
+                      {data.originalurl}
+                    </div>
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4 text-sm text-slate-300">
+                    {baseUrl + data.shorturl}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4 text-right text-sm font-medium">
+                    <button
+                      onClick={() => (
+                        handleCopy(baseUrl + data.shorturl), setIsCopied(true)
+                      )}
+                      className="cursor-pointer font-semibold text-blue-400 hover:text-blue-300"
+                    >
+                      {isCopied ? (
+                        <span className="text-green-400">✅ Copied!</span>
+                      ) : (
+                        <span className="text-blue-400">📋 Copy</span>
+                      )}
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
